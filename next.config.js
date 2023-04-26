c8880a1d1c18890fc086fd349c886084af6f6e68
ogaoga <tsutomu.ogasawara+github@gmail.com>
2023-04-27 03:15:06 +0900

    Fix for icotile.ogaoga.org

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const prefixPath = '/icotile.ogaoga.org';
+// const prefixPath = '/icotile.ogaoga.org';
+const prefixPath = '';
 
 module.exports = withBundleAnalyzer({
   poweredByHeader: false,
