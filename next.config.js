ca7de1434ee521c97dbeed719a5e001bbdc7839e
ogaoga <tsutomu.ogasawara+github@gmail.com>
2023-04-27 02:13:27 +0900

    Add prefixPath

--- next.config.js
+++ next.config.js
@@ -3,10 +3,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const prefixPath = '/icotile.ogaoga.org';
+
 module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: '',
+  basePath: prefixPath,
+  assetPrefix: prefixPath,
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
